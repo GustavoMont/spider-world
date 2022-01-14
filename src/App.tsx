42600793a0b83b12acecb20fde22b69eeeedafad
GustavoMont <lgmluisgm@gmail.com>
2022-01-14 18:58:20 -0300

    :tada: initial commit
    Remove useless files, add md5 and dotenv dependences.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
 
-function App() {
+const App: React.FC = () => {
+
+  useEffect(() =>{
+    (async () =>{
+      const req = await fetch('https://api.github.com/users/gustavomont');
+      const userInfo = await req.json()
+      console.log(userInfo);
+    })()
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
