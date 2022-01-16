b060bfbf39457f62e0e4d03ca61907ff1e3dc7ba
GustavoMont <lgmluisgm@gmail.com>
2022-01-16 00:42:41 -0300

    :pencil: update color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    colors: {
+      'red': 'rgb(223, 31, 45)',
+      'dark-red': 'rgb(177, 19, 19)',
+      'blue': 'rgb(43, 55, 132)',
+      'white': '#fff',
+      black: {
+        100: '#050505',
+        80: 'rgba(0, 0, 0, 0.8)',
+        70: 'rgba(0, 0, 0, 0.7)',
+        50: 'rgba(0, 0, 0, 0.5)'
+      },
+      transparent: 'transparent'
+    },
+    extend: {
+    },
   },
   plugins: [],
 }
