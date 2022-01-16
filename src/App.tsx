6f645fa973ef4fcfcbe7bcc1993a90e7bbe3c612
GustavoMont <lgmluisgm@gmail.com>
2022-01-16 00:43:38 -0300

    :pencil2: make a spidey uniforme on background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import AppRoutes from './routes';
+import React from "react";
+import AppRoutes from "./routes";
 
 const App: React.FC = () => {
-
   return (
-    <AppRoutes />
+    <>
+      <div className="bg-dark-red -z-50 relative min-h-screen">
+        <i className="fas fa-spider -z-50 darkmode fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[250px] dark:text-white"></i>
+        <AppRoutes />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
