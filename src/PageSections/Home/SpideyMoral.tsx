ee524a115837ad7aa01c60c8340465b33be78ac6
GustavoMont <lgmluisgm@gmail.com>
2022-01-17 14:08:00 -0300

    :pencil: update all home page sections

--- src/PageSections/Home/SpideyMoral.tsx
+++ src/PageSections/Home/SpideyMoral.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 function SpideyMoral() {
   return (
-    <div id="sentence" className="text-white text-center">
-      <h2 className="text-[2rem]" id="powers">
-        With Great Powers
-      </h2>
-      <p className="my-5  dark:text-blue">comes</p>
-      <h2 className="text-[2rem]" id="resp">
-        Great Responsibility
-      </h2>
-    </div>
+    <section
+      className="h-screen flex items-center justify-center"
+      id="power-resp"
+    >
+      <div id="sentence" className="text-white text-center">
+        <h2 className="text-[2rem]" id="powers">
+          With Great Powers
+        </h2>
+        <p className="my-5  dark:text-blue">comes</p>
+        <h2 className="text-[2rem]" id="resp">
+          Great Responsibility
+        </h2>
+      </div>
+    </section>
   );
 }
 
