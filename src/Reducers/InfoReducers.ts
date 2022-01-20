b777c501525920fc4718f3d8c0856ffb355115fe
GustavoMont <lgmluisgm@gmail.com>
2022-01-20 15:30:42 -0300

    :pencil: comentt reducer

--- src/Reducers/InfoReducers.ts
+++ src/Reducers/InfoReducers.ts
@@ -3,7 +3,7 @@ import { SpideyContent } from "../Types/SpideyContent";
 export interface Action {
   type: "ALL_INFO";
   info: SpideyContent;
-}
+} // Ele lida com as informações dos quadrinhos que serão vistas na sua página
 
 export function InfoReducer(state: SpideyContent, action: Action) {
   switch (action.type) {
