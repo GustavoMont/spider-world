70bc2461a55363bc769edafde7cc0f35a27b2b8a
GustavoMont <lgmluisgm@gmail.com>
2022-01-20 16:22:06 -0300

    :pencil: make background a component

---
+++ src/Components/SpiderSuit.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SpiderSuit = () => {
+  return (
+    <div className="bg-dark-red dark:bg-black-100 fixed top-0 ease-in duration-300 left-0 w-screen h-screen -z-50 flex items-center justify-center">
+      <i className="fas fa-spider text-[10rem] ease-in duration-500 dark:text-white"></i>
+    </div>
+  );
+};
+
+export default SpiderSuit;
